de8a45d9cdadad58f30021674bb7b30bd05a8678
VP859 <mierzwa.jan.6@gmail.com>
2023-11-23 12:36:31 +0100

    updated generations and added new functions

--- Trigonometric_functions.py
+++ Trigonometric_functions.py
@@ -80,6 +80,41 @@ class Trigonometric:
         plt.title('Arctan function')
 
         plt.savefig('figures/arctan_function.png', format='png', dpi=300)
+    
+
+    
+    def arcsin(self):
+        data = np.sort(self.data)
+
+        values = np.arcsin(data)
+
+        fig, ax = plt.subplots()
+        ax.plot(data, values)
+
+        ax.set_title('Arcsin function')
+        ax.set_xlabel('Values')
+        ax.set_ylabel('Arcsin values')
+
+        plt.title('Arcsin function')
+
+        plt.savefig('figures/arcsin_function.png', format='png', dpi=300)
+
+    
+    def arccos(self):
+        data = np.sort(self.data)
+
+        values = np.arccos(data)
+
+        fig, ax = plt.subplots()
+        ax.plot(data, values)
+
+        ax.set_title('Arccos function')
+        ax.set_xlabel('Values')
+        ax.set_ylabel('Arccos values')
+
+        plt.title('Arccos function')
+
+        plt.savefig('figures/arccos_function.png', format='png', dpi=300)
 
 
     def display(self):
@@ -95,46 +130,30 @@ class Trigonometric:
                     data.append(float(element))
 
         self.data = np.array(data)
+    
+
+    def generate(self, size = 1000):
+        self.data = np.random.randint(0, 90, size)
+
+        self.data = np.radians(self.data)
+
         
 
+    
 
-    def generate(self):
-        data = []
-        size = np.random.randint(1000, 10000)
-        
-
-        for element in range(size):
-                data.append(np.random.randint(-100, 100))
-                
-
-                
-        
-        self.data = np.array(data)
-        
-        with open('dataset1.csv', 'w') as file:
-            i = 0
-            for element in self.data:
-                if i == 5:
-                    i = 0
-                    file.write(f'\n')
-                file.write(f'{element},')
-                i += 1
-
-
-            
 
 
 if __name__ == '__main__':
     start = process_time()
 
-    functions = Trigonometric('trigonometric_dataset.csv')
+    functions = Trigonometric()
 
     functions.sin()
     functions.cos()
     functions.tan()
     functions.arctan()
-
-
+    functions.arcsin()
+    functions.arccos()
 
     end = process_time()
 
